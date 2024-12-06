6c4199a156c33ccecaf21d707f51b7fcbf044b1e
Yash Visavadiya <yashvishavadia6996@gmail.com>
2024-12-06 23:31:58 +0530

    Added Guard Gallivant Problem - Solution - 2.

--- Day 6/Guard Gallivant.py
+++ Day 6/Guard Gallivant.py
@@ -1,5 +1,3 @@
-from os import startfile
-
 def read_input():
     with open("input.txt", 'r') as file:
         lines = file.readlines()
@@ -9,6 +7,7 @@ def read_input():
             grid.append(list(line.strip()))
 
     return grid
+
 
 def guard_walk(x, y, di):
     if not in_bounds(x, y):
@@ -21,7 +20,6 @@ def guard_walk(x, y, di):
         G[x][y] = 'X'
         dx, dy = get_next(x, y, di)
 
-
     if in_bounds(dx, dy) and G[dx][dy] == '#':
         guard_walk(x, y, (di + 1) % 4)
 
@@ -29,25 +27,41 @@ def guard_walk(x, y, di):
 def in_bounds(x, y):
     return R > x >= 0 <= y < C
 
-def get_previous(x, y, di):
-    return x - d[di][0], y - d[di][1]
 
 def get_next(x, y, di):
     return x + d[di][0], y + d[di][1]
 
-def get_next_with_di_change(x, y, di):
-    return x + d[(di + 1) % 4][0], y + d[(di + 1) % 4][1]
 
 G = read_input()
 R, C = len(G), len(G[0])
 d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-ans = [0]
+ans = 0
+st = end = 0
 
 for r in range(R):
     for c in range(C):
         if G[r][c] == '^':
-            G[r][c] = 'X'
-            guard_walk(r, c, 0)
+            st, end = r, c
 
-[print(''.join(row)) for row in G]
-print(sum([row.count('X') for row in G]))
+for o_r in range(R):
+    for o_c in range(C):
+        r, c = st, end
+        di = 0
+        seen = set()
+
+        while True:
+            if (r, c, di) in seen:
+                ans += 1
+                break
+            seen.add((r, c, di))
+            rr, cc = get_next(r, c, di)
+
+            if not in_bounds(rr, cc):
+                break
+
+            if G[rr][cc] == '#' or (rr, cc) == (o_r, o_c):
+                di = (di + 1) % 4
+            else:
+                r, c = rr, cc
+
+print(ans)
